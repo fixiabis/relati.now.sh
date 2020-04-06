884c7d8a438ca85b3bb728f27bd3fa4c2238355b
fixiabis <fixiabis@gmail.com>
2020-04-07 01:15:29 +0800

    add animation

--- pages/how-to-play.tsx
+++ pages/how-to-play.tsx
@@ -6,10 +6,20 @@ import { Page, Board, Button, IconButton } from "../components";
 
 const HowToPlay = () => {
   const router = useRouter();
+  const [progress, setProgess] = useState(0);
 
   return (
     <Page id="how-to-play" title="How to play">
-      <Board width={9} height={9} />
+      <Board width={9} height={9}>
+        <path d="
+          M 21 21 m  0  1 v -1 h  1
+          M 21 24 m  0 -1 v  1 h  1
+          M 24 21 m  0  1 v -1 h -1
+          M 24 24 m  0 -1 v  1 h -1
+        " stroke="crimson" stroke-width="0.4px" fill="none" style={{
+          animation: "point-highlight 1s infinite"
+        }}></path>
+      </Board>
       <Button.Group>
         <IconButton type="leave" color="#888" onClick={() => router.push("/")} />
       </Button.Group>
