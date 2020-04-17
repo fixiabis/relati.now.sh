4fe0b6a9bfe3f1efcf38c1d89155a9a733872388
fixiabis <fixiabis@gmail.com>
2020-04-17 21:05:54 +0800

    performance improve

--- components/Relati/RelatiTutorial/RelatiTutorial.tsx
+++ components/Relati/RelatiTutorial/RelatiTutorial.tsx
@@ -5,7 +5,12 @@ const RelatiTutorial = () => {
   const [step, setStep] = useState(0);
   const nextStep = () => setStep(step + 1);
   const Scene = RELATI_SCENES[step];
-  return <Scene nextStep={nextStep} />;
+  const { innerWidth = 45, innerHeight = 165 } = globalThis;
+  const widthRatio = innerWidth / 45;
+  const heightRatio = (innerHeight - 120) / 45;
+  const scale = Math.min(widthRatio, heightRatio) * 0.95;
+  const style = { transform: `scale(${scale})` };
+  return <Scene nextStep={nextStep} style={style} />;
 };
 
 export default RelatiTutorial;
