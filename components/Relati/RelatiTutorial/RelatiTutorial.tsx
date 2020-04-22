4cc03a2bbf15adc73e036cd3b52114db35b5d040
fixiabis <fixiabis@gmail.com>
2020-04-23 01:22:08 +0800

    set style after render

--- components/Relati/RelatiTutorial/RelatiTutorial.tsx
+++ components/Relati/RelatiTutorial/RelatiTutorial.tsx
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import RELATI_SCENES from "./scenes";
 
 const RelatiTutorial = () => {
   const [step, setStep] = useState(0);
+  const [scale, setScale] = useState(0.95);
   const nextStep = () => setStep(step + 1);
   const Scene = RELATI_SCENES[step];
-  const { innerWidth = 45, innerHeight = 165 } = globalThis;
-  const widthRatio = innerWidth / 45;
-  const heightRatio = (innerHeight - 120) / 45;
-  const scale = Math.min(widthRatio, heightRatio) * 0.95;
   const style = { transform: `scale(${scale})` };
+
+  useEffect(() => {
+    const { innerWidth = 45, innerHeight = 185 } = globalThis;
+    const widthRatio = innerWidth / 45;
+    const heightRatio = (innerHeight - 140) / 45;
+    const scale = Math.min(widthRatio, heightRatio) * 0.95;
+    setScale(scale);
+  }, []);
 
   return (
     <div className="relati-tutorial">
