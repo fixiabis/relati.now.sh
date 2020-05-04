f29c843f3fc4d9cac924003d993fc833218fd09e
fixiabis <fixiabis@gmail.com>
2020-05-04 12:39:30 +0800

    refactor

--- pages/play-lite.tsx
+++ pages/play-lite.tsx
@@ -52,14 +52,16 @@ const PlayLite = () => {
       : undefined;
 
   const saveGame = () => {
-    const placementRecordJSONText = JSON.stringify(game.placementRecords);
+    const placementRecords = game.placementRecords.map(([x, y]) => game.board.getGridAt(x, y)?.i || -1);
+    const placementRecordsJSONText = JSON.stringify(placementRecords);
     const fileType = "text/json";
-    const file = new Blob([placementRecordJSONText], { type: fileType });
+    const file = new Blob([placementRecordsJSONText], { type: fileType });
     const fileUrl = URL.createObjectURL(file);
     const nowTime = Date.now();
-    const fileName = `relati-record-${nowTime}.json`;
+    const fileName = `relati-lite-record-at-${nowTime}.json`;
     const link = document.createElement("a");
     link.href = fileUrl;
+    link.target = "_blank";
     link.download = fileName;
     document.body.appendChild(link);
     link.click();
