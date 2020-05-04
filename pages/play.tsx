f75b16531208739694a6472bc2c69cfd6455b2b4
fixiabis <fixiabis@gmail.com>
2020-05-04 12:19:15 +0800

    refactor

--- pages/play.tsx
+++ pages/play.tsx
@@ -9,15 +9,16 @@ import { useForceUpdate } from "../utils/hook";
 
 const Play = () => {
   const router = useRouter();
-  const [game] = useState<Game>(new Game(2));
   const forceUpdate = useForceUpdate();
+  const [game] = useState<Game>(new Game(2));
   const [isGameOverMessageBoxShow, setIsGameOverMessageBoxShow] = useState(true);
   const [isGameLeaveMessageBoxShow, setIsGameLeaveMessageBoxShow] = useState(false);
   const gameSetting = useSelector<State, SettingState>(state => state.setting);
   const gameOnOver = forceUpdate;
-  const gameLeaveMessageBoxOpen = () => setIsGameLeaveMessageBoxShow(true);
-  const gameOverMessageBoxClose = () => setIsGameOverMessageBoxShow(false);
-  const gameLeaveMessageBoxClose = () => setIsGameLeaveMessageBoxShow(false);
+  const leavePage = () => router.replace("/");
+  const openGameLeaveMessageBox = () => setIsGameLeaveMessageBoxShow(true);
+  const closeGameOverMessageBox = () => setIsGameOverMessageBoxShow(false);
+  const closeGameLeaveMessageBox = () => setIsGameLeaveMessageBoxShow(false);
 
   const restartGame = () => {
     game.restart();
@@ -26,42 +27,86 @@ const Play = () => {
 
   const leaveGame = () => {
     if (game.turn && game.symbolOfWinner === "?") {
-      gameLeaveMessageBoxOpen();
+      openGameLeaveMessageBox();
     }
     else {
-      router.replace("/");
+      leavePage();
     }
   };
 
-  const messageIcon = game.symbolOfWinner !== "?"
-    ? (
-      <div className="message-icon-container">
-        <svg width="5" height="5" className="message-icon">
-          <RelatiPiece x={0} y={0} symbol={game.symbolOfWinner} primary />
-        </svg>
-      </div>
-    )
-    : undefined;
+  const messageIcon =
+    game.symbolOfWinner !== "?"
+      ? (
+        <div className="message-icon-container">
+          <svg width="5" height="5" className="message-icon">
+            <RelatiPiece x={0} y={0} symbol={game.symbolOfWinner} primary />
+          </svg>
+        </div>
+      )
+      : undefined;
 
-  const messageText = game.symbolOfWinner !== "?"
-    ? game.symbolOfWinner !== "N"
-      ? `${game.turn % 2 ? "藍" : "紅"}方玩家獲勝!`
-      : "平手!"
-    : "";
+  const messageText =
+    game.symbolOfWinner !== "?"
+      ? game.symbolOfWinner !== "N"
+        ? `${game.turn % 2 ? "藍" : "紅"}方玩家獲勝!`
+        : "平手!"
+      : undefined;
 
   const saveGame = () => {
-    const file = new Blob([JSON.stringify(game.placementRecords)], { type: "text/json" });
+    const placementRecordJSONText = JSON.stringify(game.placementRecords);
+    const fileType = "text/json";
+    const file = new Blob([placementRecordJSONText], { type: fileType });
     const fileUrl = URL.createObjectURL(file);
+    const nowTime = Date.now();
+    const fileName = `relati-record-${nowTime}.json`;
     const link = document.createElement("a");
     link.href = fileUrl;
-    link.download = "relati-record.json";
+    link.download = fileName;
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
   };
 
+  const gameLeaveMessageBox =
+    isGameLeaveMessageBoxShow
+      ? (
+        <MessageBox onCancel={closeGameLeaveMessageBox}>
+          <div className="message-container">
+            <div className="message-icon-container">
+              <svg width="5" height="5" className="message-icon">
+                <RelatiPiece x={0} y={0} symbol="K" primary />
+              </svg>
+            </div>
+          勝負未分, 確定離開?
+        </div>
+          <Button.Group>
+            <IconButton type="accept" color="crimson" onClick={leavePage} />
+            <IconButton type="download" color="#888" onClick={saveGame} />
+            <IconButton type="reject" color="royalblue" onClick={closeGameLeaveMessageBox} />
+          </Button.Group>
+        </MessageBox>
+      )
+      : undefined;
+
+  const gameOverMessageBox =
+    isGameOverMessageBoxShow && game.symbolOfWinner !== "?"
+      ? (
+        <MessageBox onCancel={closeGameOverMessageBox}>
+          <div className="message-container">
+            {messageIcon}
+            {messageText}
+          </div>
+          <Button.Group>
+            <IconButton type="retry" color="crimson" onClick={restartGame} />
+            <IconButton type="download" color="#888" onClick={saveGame} />
+            <IconButton type="reject" color="royalblue" onClick={leaveGame} />
+          </Button.Group>
+        </MessageBox>
+      )
+      : undefined;
+
   return (
-    <Page id="play" title="play lite">
+    <Page id="play" title="play">
       <div className="versus-header">
         <div className="player-o" />
         <div className="versus" />
@@ -74,33 +119,8 @@ const Play = () => {
         <IconButton type="leave" color="#888" onClick={leaveGame} />
       </Button.Group>
 
-      <MessageBox show={isGameLeaveMessageBoxShow} onCancel={gameLeaveMessageBoxClose}>
-        <div className="message-container">
-          <div className="message-icon-container">
-            <svg width="5" height="5" className="message-icon">
-              <RelatiPiece x={0} y={0} symbol="K" primary />
-            </svg>
-          </div>
-          勝負未分, 確定離開?
-        </div>
-        <Button.Group>
-          <IconButton type="accept" color="crimson" onClick={() => router.replace("/")} />
-          <IconButton type="download" color="#888" onClick={saveGame} />
-          <IconButton type="reject" color="royalblue" onClick={gameLeaveMessageBoxClose} />
-        </Button.Group>
-      </MessageBox>
-
-      <MessageBox show={isGameOverMessageBoxShow && game.symbolOfWinner !== "?"} onCancel={gameOverMessageBoxClose}>
-        <div className="message-container">
-          {messageIcon}
-          {messageText}
-        </div>
-        <Button.Group>
-          <IconButton type="retry" color="crimson" onClick={restartGame} />
-          <IconButton type="download" color="#888" onClick={saveGame} />
-          <IconButton type="reject" color="royalblue" onClick={leaveGame} />
-        </Button.Group>
-      </MessageBox>
+      {gameLeaveMessageBox}
+      {gameOverMessageBox}
     </Page>
   );
 };
--- utils/common/index.ts
+++ utils/common/index.ts
@@ -1,1 +1,10 @@
 export const delay = (ms: number) => new Promise(finishDelay => setTimeout(finishDelay, ms));
+
+export const formatDate = (date: Date) => [
+    date.getFullYear(), "-",
+    date.getMonth(), "-",
+    date.getDate(), " ",
+    date.getHours(), ":",
+    date.getMinutes(), ":",
+    date.getSeconds(),
+].map((v, i) => i % 2 || v > 9 ? v : `0${v}`).join("");
