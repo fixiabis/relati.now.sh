dd29a0909ae38d80cca417d850e6dda9b2f11c14
fixiabis <fixiabis@gmail.com>
2020-05-22 01:45:25 +0800

    add player button

--- pages/choose-game-mode.tsx
+++ pages/choose-game-mode.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { useRouter } from "next/router";
-import { Page, Button, IconButton } from "../components";
+import { Page, Button, IconButton, RelatiPiece } from "../components";
 
 const ChooseGameMode = () => {
   const router = useRouter();
   const leavePage = () => router.replace("/");
-  const toPlay2POnX5Page = () => router.replace("/play-2p-on-x5");
-  const toPlay2POnX9Page = () => router.replace("/play-2p-on-x9");
+  const toPlay2pOnX5Page = () => router.replace("/play-2p-on-x5");
+  const toPlay1pOnX5WithOPage = () => router.replace("/play-1p-on-x5-with-o-lv1");
+  const toPlay1pOnX5WithXPage = () => router.replace("/play-1p-on-x5-with-x-lv1");
+  const toPlay2pOnX9Page = () => router.replace("/play-2p-on-x9");
 
   return (
     <Page id="choose-game-mode" title="choose game mode">
@@ -14,10 +16,22 @@ const ChooseGameMode = () => {
         <div className="play-icon" />
         請選擇模式
         <Button.Group>
-          <IconButton type="x5" color="royalblue" className="with-text" onClick={toPlay2POnX5Page}>
+          <IconButton type="x5" color="royalblue" className="with-text" onClick={toPlay2pOnX5Page}>
             單機雙人
           </IconButton>
-          <IconButton type="x9" color="crimson" className="with-text" onClick={toPlay2POnX9Page}>
+          <IconButton type="x5" color="royalblue" className="with-text" onClick={toPlay1pOnX5WithOPage}>
+            單機對
+            <svg width="5" height="5" style={{ transform: "scale(4)", marginLeft: 7.5 }}>
+              <RelatiPiece x={0} y={0} symbol="O" primary />
+            </svg>
+          </IconButton>
+          <IconButton type="x5" color="royalblue" className="with-text" onClick={toPlay1pOnX5WithXPage}>
+            單機對
+            <svg width="5" height="5" style={{ transform: "scale(4)", marginLeft: 7.5 }}>
+              <RelatiPiece x={0} y={0} symbol="X" primary />
+            </svg>
+          </IconButton>
+          <IconButton type="x9" color="crimson" className="with-text" onClick={toPlay2pOnX9Page}>
             單機雙人
           </IconButton>
         </Button.Group>
