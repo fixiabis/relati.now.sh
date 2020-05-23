6dc739a61eedb56d7b51157675eb87f06be9cf0b
fixiabis <fixiabis@gmail.com>
2020-05-23 19:14:12 +0800

    fix type bug

--- components/Relati/index.ts
+++ components/Relati/index.ts
@@ -1,7 +1,7 @@
 export { default as RelatiGame } from "./RelatiGame";
-export type { RelatiGameProps } from "./RelatiGame";
+export type { Props as RelatiGameProps } from "./RelatiGame";
 export { default as RelatiBoard } from "./RelatiBoard";
-export type { RelatiBoardProps } from "./RelatiBoard";
+export type { Props as RelatiBoardProps } from "./RelatiBoard";
 export { default as RelatiPiece } from "./RelatiPiece";
-export type { RelatiPieceProps } from "./RelatiPiece";
+export type { Props as RelatiPieceProps } from "./RelatiPiece";
 export { default as RelatiTutorial } from "./RelatiTutorial";
