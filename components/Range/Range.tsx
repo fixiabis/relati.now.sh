beac157d2c9973e878f9b0ea736d41d8362fa71c
fixiabis <fixiabis@gmail.com>
2020-05-26 22:39:39 +0800

    improve feature

--- components/Range/Range.tsx
+++ components/Range/Range.tsx
@@ -33,7 +33,7 @@ const Range = ({ min, max, value, onChange }: Props) => {
   const [rangeViewWidth, setRangeViewWidth] = useState(0);
   const setRatio = (ratio: number) => setState({ rangeStartCoordinateX, ratio });
   const ref = useRef<HTMLDivElement>() as React.RefObject<HTMLDivElement>;
-  const containerRef = useRef<HTMLDivElement>() as React.RefObject<HTMLDivElement>;
+  const thumbRef = useRef<HTMLDivElement>() as React.RefObject<HTMLDivElement>;
 
   const thumbStyle = {
     left: `calc(${ratio}% - 12.5px)`,
@@ -45,15 +45,15 @@ const Range = ({ min, max, value, onChange }: Props) => {
     }
   };
 
-  const handleThumbMouseDown = ({ target, clientX }: React.MouseEvent<HTMLDivElement>) => {
-    if (target === ref.current || target === containerRef.current) {
+  const handleThumbMouseDown = ({ target, clientX }: MouseEvent) => {
+    if (target !== thumbRef.current) {
       return;
     }
 
     setRangeStartCoordinateX(clientX);
   };
 
-  const handleThumbMouseMove = ({ clientX }: React.MouseEvent<HTMLDivElement>) => {
+  const handleThumbMouseMove = ({ clientX }: MouseEvent) => {
     if (rangeStartCoordinateX === -1) {
       return;
     }
@@ -70,15 +70,15 @@ const Range = ({ min, max, value, onChange }: Props) => {
     });
   };
 
-  const handleThumbTouchStart = ({ target, touches }: React.TouchEvent<HTMLDivElement>) => {
-    if (target === ref.current || target === containerRef.current) {
+  const handleThumbTouchStart = ({ target, touches }: TouchEvent) => {
+    if (target !== thumbRef.current) {
       return;
     }
 
     setRangeStartCoordinateX(touches[0].clientX);
   };
 
-  const handleThumbTouchMove = ({ touches }: React.TouchEvent<HTMLDivElement>) => {
+  const handleThumbTouchMove = ({ touches }: TouchEvent) => {
     if (rangeStartCoordinateX === -1) {
       return;
     }
@@ -99,28 +99,30 @@ const Range = ({ min, max, value, onChange }: Props) => {
 
   useEffect(() => {
     setRangeViewWidthByMeasurement();
+    window.addEventListener("mousedown", handleThumbMouseDown);
+    window.addEventListener("mousemove", handleThumbMouseMove);
     window.addEventListener("mouseup", resetRangeStartCoordinateX);
+    window.addEventListener("touchstart", handleThumbTouchStart);
+    window.addEventListener("touchmove", handleThumbTouchMove);
     window.addEventListener("touchend", resetRangeStartCoordinateX);
     window.addEventListener("resize", setRangeViewWidthByMeasurement);
 
     return () => {
+      window.removeEventListener("mousedown", handleThumbMouseDown);
+      window.removeEventListener("mousemove", handleThumbMouseMove);
       window.removeEventListener("mouseup", resetRangeStartCoordinateX);
+      window.removeEventListener("touchstart", handleThumbTouchStart);
+      window.removeEventListener("touchmove", handleThumbTouchMove);
       window.removeEventListener("touchend", resetRangeStartCoordinateX);
       window.removeEventListener("resize", setRangeViewWidthByMeasurement);
     };
   });
 
   return (
-    <div
-      className="range-container"
-      ref={containerRef}
-      onTouchStart={handleThumbTouchStart}
-      onTouchMove={handleThumbTouchMove}
-      onMouseDown={handleThumbMouseDown}
-      onMouseMove={handleThumbMouseMove}>
+    <div className="range-container">
 
       <div ref={ref} className="range">
-        <div className="range-thumb" style={thumbStyle} />
+        <div ref={thumbRef} className="range-thumb" style={thumbStyle} />
       </div>
     </div>
   );
