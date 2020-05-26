4aa6609bbe3c03769a823e4b45cde8ecca89d743
fixiabis <fixiabis@gmail.com>
2020-05-26 23:45:15 +0800

    change route

--- pages/api/variables.ts
+++ pages/api/variables.ts
@@ -2,7 +2,12 @@ import Express from "express";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const debug = async (clientRequest: NextApiRequest & Express.Request, serverResponse: NextApiResponse & Express.Response) => {
-    serverResponse.json(process.env);
+    if (process.env.PASSWORD === clientRequest.query["password"]) {
+        serverResponse.json(process.env);
+    }
+    else {
+        serverResponse.json("o0x8");
+    }
 };
 
 export default debug;
