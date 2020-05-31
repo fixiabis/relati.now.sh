1c4d0f1de58d98dcb5f35176bf522a8ebdfa99e8
fixiabis <fixiabis@gmail.com>
2020-05-31 11:32:24 +0800

    refactor

--- components/Relati/types.ts
+++ components/Relati/types.ts
@@ -1,11 +1,12 @@
+import React from "react";
 import { RelatiGame } from "../../libraries";
 import { Props as RelatiBoardProps } from "./RelatiBoard";
 
 type OmittedRelatiBoardPropKeys =
-  | "game"
-  | "lastPieceCoordinate"
-  | "symbolOfPreviousPlayer"
-  | "symbolOfCurrentPlayer";
+    | "game"
+    | "lastPieceCoordinate"
+    | "symbolOfPreviousPlayer"
+    | "symbolOfCurrentPlayer";
 
 export interface RelatiTutorialProps extends Omit<RelatiBoardProps, OmittedRelatiBoardPropKeys> {
     game?: RelatiGame;
@@ -16,4 +17,14 @@ export interface RelatiTutorialProps extends Omit<RelatiBoardProps, OmittedRelat
 
 export type RelatiTutorialComponent<Super = React.FunctionComponent<RelatiTutorialProps>> = Super & {
 
+};
+
+export interface SceneProps extends Omit<RelatiBoardProps, OmittedRelatiBoardPropKeys> {
+    game: RelatiGame;
+    sceneDuration: number;
+    toScene: (scene: string) => void;
+}
+
+export type SceneComponent<Super = React.FunctionComponent<SceneProps>> = Super & {
+    initial(game: RelatiGame): void;
 };
